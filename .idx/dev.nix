a79ec89e5eaea1d81df87e4b14436d87e8d1d251
infrastructure-cloud-grit <infrastructure.cloud.grit@gmail.com>
2025-08-21 16:48:30 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,43 +1,17 @@
 { pkgs, ... }: {
 
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.nodejs_20
-  ];
-
-  # Sets environment variables in the workspace
-  env = {
-    SOME_ENV_VAR = "hello";
-  };
-
-  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-  idx.extensions = [
-    "angular.ng-template"
-  ];
+  # NOTE: This is an excerpt of a complete Nix configuration example.
+  # For more information about the dev.nix file in Firebase Studio, see
+  # https://firebase.google.com/docs/studio/customize-workspace
 
   # Enable previews and customize configuration
   idx.previews = {
     enable = true;
-    previews = {
-      web = {
-        command = [
-          "npm"
-          "run"
-          "start"
-          "--"
-          "--port"
-          "$PORT"
-          "--host"
-          "0.0.0.0"
-          "--disable-host-check"
-        ];
+    previews.web = {
+        command = [ "${pkgs.python3}/bin/python" "-m" "http.server" "$PORT" ];
         manager = "web";
         # Optionally, specify a directory that contains your web app
         # cwd = "app/client";
-      };
     };
   };
 }
